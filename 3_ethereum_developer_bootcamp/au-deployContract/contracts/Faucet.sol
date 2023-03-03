e3dafb52fbe08b96c0a0638766469d0583cd0b04
jeeva <jeeva.ramu@codurance.com>
2023-03-03 11:49:49 +0100

    added a demo contract to deply to testnet

---
+++ 3_ethereum_developer_bootcamp/au-deployContract/contracts/Faucet.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Faucet {
+  
+  function withdraw(uint _amount) public {
+    require(_amount <= 100000000000);
+    payable(msg.sender).transfer(_amount);
+  }
+
+  receive() external payable {}
+}
